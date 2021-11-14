bdebbddb268584ca7637cbc4aa2ee095b342f5d4
Zehra  Erdem <zehra.erdem@ug.bilkent.edu.tr>
2021-11-14 21:14:17 +0300

    Event table first step

--- backend/src/main/java/luna/clubverse/backend/event/entity/Event.java
+++ backend/src/main/java/luna/clubverse/backend/event/entity/Event.java
@@ -1,17 +1,67 @@
 package luna.clubverse.backend.event.entity;
 
 import luna.clubverse.backend.common.entity.BaseEntity;
+import luna.clubverse.backend.event.entity.enumuration.EventStatus;
+import luna.clubverse.backend.financedata.entity.FinanceData;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "event")
 public class Event extends BaseEntity {
 
     private String name;
 
     private String description;
 
+    private EventStatus eventStatus;
 
+    private int gePoint;
 
+    private LocalDate startDate;
 
+    private LocalDate endDate;
+
+    private LocalDate registrationDeadline;
+
+    private LocalDate reviewDeadline;
+
+    private int quota;
+
+    private int remainingQuota;
+
+    private boolean memberOnly;
+
+    private int totalPoint;
+
+    private int numberEvaluation;
+
+    @OneToOne()
+    @JoinColumn(name = "finance_data_id")
+    private FinanceData financeData;
+
+    protected Event() {
+    }
+
+    public Event(String name, String description, EventStatus eventStatus, int gePoint, LocalDate startDate, LocalDate endDate, LocalDate registrationDeadline, LocalDate reviewDeadline, int quota, boolean memberOnly, FinanceData financeData) {
+        this.name = name;
+        this.description = description;
+        this.eventStatus = eventStatus;
+        this.gePoint = gePoint;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.registrationDeadline = registrationDeadline;
+        this.reviewDeadline = reviewDeadline;
+        this.quota = quota;
+        this.remainingQuota = quota;
+        this.memberOnly = memberOnly;
+        this.totalPoint = 0;
+        this.numberEvaluation = 0;
+        this.financeData = financeData;
+    }
 }
--- backend/src/main/java/luna/clubverse/backend/financedata/entity/FinanceData.java
+++ backend/src/main/java/luna/clubverse/backend/financedata/entity/FinanceData.java
@@ -4,22 +4,37 @@ package luna.clubverse.backend.financedata.entity;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import luna.clubverse.backend.common.entity.BaseEntity;
+import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Accessors(fluent = true)
+@Table(name = "finance_data")
 public class FinanceData extends BaseEntity {
 
+    @Column(name = "amount_of_money")
     private double amountOfMoney;
 
+    @Column(name = "status")
     private String status;
 
+    @Column(name = "explanation")
     private String explanation;
 
-    private String date;
+    @Column(name = "date")
+    private LocalDate date;
 
+    protected FinanceData() {
+    }
 
-
+    public FinanceData(double amountOfMoney, String status, String explanation, LocalDate date) {
+        this.amountOfMoney = amountOfMoney;
+        this.status = status;
+        this.explanation = explanation;
+        this.date = date;
+    }
 }
