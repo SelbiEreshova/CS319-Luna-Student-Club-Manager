eebc0995fff531512e3be76d1bc72d4cd16f41e9
Zehra  Erdem <zehra.erdem@ug.bilkent.edu.tr>
2021-11-14 20:25:44 +0300

    BaseEntitiy is created

---
+++ backend/src/main/java/luna/clubverse/backend/event/entity/Event.java
@@ -0,0 +1,15 @@
+package luna.clubverse.backend.event.entity;
+
+import luna.clubverse.backend.common.entity.BaseEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Event extends BaseEntity {
+
+    private String name;
+
+    private String description;
+
+
+}
