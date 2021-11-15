71eafb8968b870dfcb6da90f5e425255be5588c4
Zehra  Erdem <zehra.erdem@ug.bilkent.edu.tr>
2021-11-15 15:34:46 +0300

    Update EventController.java

--- backend/src/main/java/luna/clubverse/backend/event/controller/EventController.java
+++ backend/src/main/java/luna/clubverse/backend/event/controller/EventController.java
@@ -4,6 +4,8 @@ import luna.clubverse.backend.event.controller.request.AddEventRequest;
 import luna.clubverse.backend.event.service.EventService;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/event")
 public class EventController {
@@ -16,7 +18,7 @@ public class EventController {
 
     @CrossOrigin
     @PostMapping("/add") // post yeni şey eklemek için yapılır
-    public String addEvent(@RequestBody AddEventRequest addEventRequest) {
+    public String addEvent(@RequestBody @Valid final AddEventRequest addEventRequest) {
         eventService.addEvent(addEventRequest.toEvent());
         return "success";
     }
