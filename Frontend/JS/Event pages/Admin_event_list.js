0e26c75998c5342e8503470605a722df9d9c1902
Ebrar <ebrar.bozkurt@ug.bilkent.edu.tr>
2021-11-17 22:02:35 +0300

    event pages

---
+++ Frontend/JS/Event pages/Admin_event_list.js
@@ -0,0 +1,5 @@
+$(function ()
+{
+    $(".nav_bar").load("../../nav_bar_admin.html");
+    $("#events_nav").addClass("active");
+});
---
+++ Frontend/JS/Event pages/Student_event_list.js
@@ -0,0 +1,5 @@
+$(function ()
+{
+    $(".nav_bar").load("../../nav_bar_student.html");
+    $("#events_nav").addClass("active");
+});
