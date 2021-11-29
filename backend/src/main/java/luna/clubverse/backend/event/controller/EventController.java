8dd66dd25bc4ae16e1f0d7cac35cf2fc629b4676
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2021-11-29 17:11:13 +0300

    Trying to call create event html

--- backend/src/main/java/luna/clubverse/backend/event/controller/EventController.java
+++ backend/src/main/java/luna/clubverse/backend/event/controller/EventController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/event")
 public class EventController {
 
-    @RequestMapping("/create")
+    @RequestMapping("/Create_event")
     public String createEventPage() {
         return "Create_event";
     }
