1d8ef450e26762253de980a2e81371c44b4a67a8
Ahmet Hakan Yılmaz <hakan.yilmaz@ug.bilkent.edu.tr>
2021-12-05 00:04:38 +0300

    changes

--- backend/src/main/java/luna/clubverse/backend/event/repository/EventRepository.java
+++ backend/src/main/java/luna/clubverse/backend/event/repository/EventRepository.java
@@ -2,7 +2,13 @@ package luna.clubverse.backend.event.repository;
 
 import luna.clubverse.backend.event.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
+
+   // @Query(name = "SELECT * FROM public.event ORDER BY id ASC ", nativeQuery=true)
+//    List<Event> findAllEvents();
 }
--- backend/src/main/java/luna/clubverse/backend/event/service/EventService.java
+++ backend/src/main/java/luna/clubverse/backend/event/service/EventService.java
@@ -2,6 +2,7 @@ package luna.clubverse.backend.event.service;
 
 import luna.clubverse.backend.club.entity.Club;
 import luna.clubverse.backend.club.repository.ClubRepository;
+import luna.clubverse.backend.event.controller.response.EventListQueryResponse;
 import luna.clubverse.backend.event.entity.Event;
 import luna.clubverse.backend.event.enumuration.EventStatus;
 import luna.clubverse.backend.event.repository.EventRepository;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Transactional
@@ -89,6 +91,14 @@ public class EventService {
     }
 
 
+    //public List<Event> getEventList()
+    //{
+   //     return eventRepository.findAllEvents();
+    //}
 
+
+    public List<EventListQueryResponse> getAllDemo() {
+        return eventRepository.findAll().stream().map(EventListQueryResponse::new).toList();
+    }
 
 }
