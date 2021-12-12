279bbfa6d5fa820706c8b0ae8e6b3f6393e8304c
Ebrar <ebrar.bozkurt@ug.bilkent.edu.tr>
2021-12-12 14:51:57 +0300

    student event list

--- backend/src/main/java/luna/clubverse/backend/event/controller/EventController.java
+++ backend/src/main/java/luna/clubverse/backend/event/controller/EventController.java
@@ -1,14 +1,28 @@
 package luna.clubverse.backend.event.controller;
 
+import luna.clubverse.backend.event.controller.response.EventListQueryResponse;
+import luna.clubverse.backend.event.service.EventService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class EventController {
 
+    private final EventService eventService;
 
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+
+    //Model kullanımı
     @RequestMapping("/event_list")
-    public String welcome() {
+    public String welcome(Model model) {
+        List<EventListQueryResponse> events = eventService.getAllDemo();
+        model.addAttribute("events", events);
         return "event_list";
     }
 }
